71b2d0183c26c3d2fcc3777fb11e29b122e1cf8b
arswe <abdurrahmansoftw@gmail.com>
2024-03-21 13:21:24 +0600

    add contact

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { ProjectsModule } from './projects/projects.module';
 import { ServiceModule } from './service/service.module';
 import { AboutModule } from './about/about.module';
+import { ContactModule } from './contact/contact.module';
 
 @Module({
-  imports: [UsersModule, ProjectsModule, ServiceModule, AboutModule],
+  imports: [UsersModule, ProjectsModule, ServiceModule, AboutModule, ContactModule],
   controllers: [AppController],
   providers: [AppService],
 })
