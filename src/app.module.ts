5e1820cbd6f16e00c134563ba93fd080855f4306
arswe <abdurrahmansoftw@gmail.com>
2024-03-23 02:41:13 +0600

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { ProjectsModule } from './modules/projects/projects.module';
-import { ServiceModule } from './service/service.module';
-import { AboutModule } from './modules/about/about.module';
-import { ContactModule } from './modules/contact/contact.module';
-import { ContactsModule } from './modules/contacts/contacts.module';
-import { ProductsModule } from './modules/products/products.module';
 
 @Module({
-  imports: [
-    UsersModule,
-    ProjectsModule,
-    ServiceModule,
-    AboutModule,
-    ContactModule,
-    ContactsModule,
-    ProductsModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
