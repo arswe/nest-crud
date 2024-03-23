cf18b76159b3ad74680e00691f33bbc9b7dbfb1f
arswe <abdurrahmansoftw@gmail.com>
2024-03-23 13:10:45 +0600

    findOneAndUpdate

--- libs/common/src/database/abstract.repository.ts
+++ libs/common/src/database/abstract.repository.ts
@@ -1,5 +1,5 @@
-import { Logger } from '@nestjs/common';
-import { Model, Types } from 'mongoose';
+import { Logger, NotFoundException } from '@nestjs/common';
+import { FilterQuery, Model, Types, UpdateQuery } from 'mongoose';
 import { AbstractDocument } from './abstract.schema';
 
 export abstract class AbstractRepository<TDocument extends AbstractDocument> {
@@ -13,5 +13,35 @@ export abstract class AbstractRepository<TDocument extends AbstractDocument> {
     });
 
     return (await createDocument.save()).toJSON as unknown as TDocument;
+  }
+
+  async findOne(filterQuery: FilterQuery<TDocument>): Promise<TDocument> {
+    const document = await this.model
+      .findOne(filterQuery)
+      .lean<TDocument>(true);
+
+    if (!document) {
+      this.logger.warn('Document was not found with filterQuery');
+      throw new NotFoundException('Document was not found');
+    }
+
+    return document;
+  }
+
+  async findOneAndUpdate(
+    filterQuery: FilterQuery<TDocument>,
+    update: UpdateQuery<TDocument>,
+  ): Promise<TDocument> {
+    const document = await this.model
+      .findOneAndUpdate(filterQuery, update, {
+        new: true,
+      })
+      .lean<TDocument>(true);
+    if (!document) {
+      this.logger.warn('Document was not found with filterQuery');
+      throw new NotFoundException('Document was not found');
+    }
+
+    return document;
   }
 }
